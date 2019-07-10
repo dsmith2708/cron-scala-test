90b340a116124f863ed5893cee098dc6f7e9c184
Daniel Smith <danielsmith.2796@googlemail.com>
2019-07-10 15:10:46 +0100

    hour and minute are now read correctly

--- src/main/scala/CronMain.scala
+++ src/main/scala/CronMain.scala
@@ -1,8 +1,25 @@
+import java.text.SimpleDateFormat
+import java.util.{Calendar, Date}
 
 object CronMain {
 
 
+  def getHourMinuteFromConsole(firstArg: String): Array[Int] = {
+      firstArg.split(":").map( stringVal => if(stringVal== "*") 100 else stringVal.toInt)
+  }
+
   def main(args: Array[String]): Unit = {
+
+    args.foreach(arg => println(arg))
+
+    val curHourMinute = getHourMinuteFromConsole(args(0))
+    curHourMinute.foreach(value => println(value))
+    val curCalendar = Calendar.getInstance()
+    if(curHourMinute(0) != 100) curCalendar.set(Calendar.HOUR, curHourMinute(0))
+    if(curHourMinute(1) != 100) curCalendar.set(Calendar.MINUTE, curHourMinute(1))
+
+
+
 
 
 
