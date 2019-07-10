8fb061c7708a0a5cae15527b7d8a725d5dee4ecb
Daniel Smith <danielsmith.2796@googlemail.com>
2019-07-10 15:50:29 +0100

    records now split and organised, file they run is recognised

--- src/main/scala/CronMain.scala
+++ src/main/scala/CronMain.scala
@@ -14,18 +14,41 @@ object CronMain {
     Source.fromFile(fileName)
   }
 
+  def splitRecordsAndOutput(configFileRecords: List[String]): Unit = {
+    configFileRecords.foreach(record => {
+      println("ITEMM")
+      val splitRecord = record.split(" ")
+      outputWhenNextRun(splitRecord)
+    })
+  }
+
+  def outputWhenNextRun(configRecord: Array[String]): Unit = {
+      configRecord match {
+        case x if x(2) == "/bin/run_me_daily" => {
+          println("run daily")
+        }
+        case x if x(2) == "/bin/run_me_hourly" => {
+          println("run hourly")
+        }
+        case x if x(2) == "/bin/run_every_minute" => {
+          println("run every minute")
+        }
+        case x if x(2) == "/bin/run_me_sixty_times" => {
+          println("run 60 times at specified time")
+        }
+        case _ => println("error")
+      }
+  }
+
+
   def main(args: Array[String]): Unit = {
 
-    args.foreach(arg => println(arg))
-
     val curHourMinute = getHourMinuteFromConsole(args(0))
-    curHourMinute.foreach(value => println(value))
     val curCalendar = Calendar.getInstance()
     if(curHourMinute(0) != 100) curCalendar.set(Calendar.HOUR, curHourMinute(0))
     if(curHourMinute(1) != 100) curCalendar.set(Calendar.MINUTE, curHourMinute(1))
 
-    val configFile = getFile(args(1)).getLines.toList
-
+    splitRecordsAndOutput(getFile(args(1)).getLines.toList)
 
 
 
